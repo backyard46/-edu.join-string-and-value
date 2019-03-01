b776e148ab61b54fb57408fd888f866fea01f644
backyarD46 <backyard46@gmail.com>
2019-03-01 10:17:34 +0900

    プロジェクト ファイルを追加します。

---
+++ JoinStringAndValue/JoinStringAndValue.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace JoinStringAndValue
+{
+    partial class JoinStringAndValue
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextResult = new System.Windows.Forms.TextBox();
+            this.ButtonGo = new System.Windows.Forms.Button();
+            this.TextResult2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TextResult
+            // 
+            this.TextResult.Location = new System.Drawing.Point(12, 31);
+            this.TextResult.Name = "TextResult";
+            this.TextResult.ReadOnly = true;
+            this.TextResult.Size = new System.Drawing.Size(248, 27);
+            this.TextResult.TabIndex = 0;
+            // 
+            // ButtonGo
+            // 
+            this.ButtonGo.Location = new System.Drawing.Point(294, 118);
+            this.ButtonGo.Name = "ButtonGo";
+            this.ButtonGo.Size = new System.Drawing.Size(120, 32);
+            this.ButtonGo.TabIndex = 1;
+            this.ButtonGo.Text = "結合";
+            this.ButtonGo.UseVisualStyleBackColor = true;
+            this.ButtonGo.Click += new System.EventHandler(this.ButtonGo_Click);
+            // 
+            // TextResult2
+            // 
+            this.TextResult2.Location = new System.Drawing.Point(12, 83);
+            this.TextResult2.Name = "TextResult2";
+            this.TextResult2.ReadOnly = true;
+            this.TextResult2.Size = new System.Drawing.Size(248, 27);
+            this.TextResult2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(359, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "TextResult.Text = \"今は\" + 2000 + 18 + \"年です\"";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(304, 19);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "TextResult2.Text = 2000 + 18 + \"年です\"";
+            // 
+            // JoinStringAndValue
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 160);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextResult2);
+            this.Controls.Add(this.ButtonGo);
+            this.Controls.Add(this.TextResult);
+            this.Font = new System.Drawing.Font("Meiryo UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "JoinStringAndValue";
+            this.Text = "文字と数値の結合";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextResult;
+        private System.Windows.Forms.Button ButtonGo;
+        private System.Windows.Forms.TextBox TextResult2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ JoinStringAndValue/JoinStringAndValue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace JoinStringAndValue
+{
+    /// <summary>
+    /// 文字と数字混在で結合した場合の挙動確認。
+    /// </summary>
+    /// <seealso cref="System.Windows.Forms.Form" />
+    public partial class JoinStringAndValue : Form
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="JoinStringAndValue"/> class.
+        /// </summary>
+        public JoinStringAndValue()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 文字と数値の結合テスト。
+        /// </summary>
+        /// <remarks>
+        /// ・C#は式の評価を左から順に行う。
+        /// ・C#では、文字と数値が足し算されると、数値は文字に自動変換される。
+        /// 　※ただし、これを活用したプログラムはあまり作らないほうが良い。
+        /// 　　後からプログラムを見た人がこのルールを熟知していないと誤読する可能性があるので。
+        /// </remarks>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ButtonGo_Click(object sender, EventArgs e)
+        {
+            // 文字(string)とintなどの数値を足し算すると、数値は文字に自動変換される
+
+            // 式は左から解釈されるので、最初に「今は2000」という文字列が作られる。
+            // 次に、「今は2000」という文字に「18」が文字として結合される。
+            TextResult.Text = "今は" + 2000 + 18 + "年です";
+
+            // 最初に「2000+18」という数値計算が行われる。
+            // 次に、2018という数字と文字の足し算なので、数字が文字に自動変換される。
+            TextResult2.Text = 2000 + 18 + "年です";
+
+        }
+    }
+}
